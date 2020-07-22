bd3def3dffb95cb56cdf4cdb4b3dc0aba581036e
Leon Marques <leomarques301@gmail.com>
2020-07-21 23:33:22 -0300

    adicionei o favicon

--- index.php
+++ index.php
@@ -4,22 +4,20 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
   case '/' :
-    require __DIR__ . '/views/index.php';
+    require __DIR__ . '/views/index.phtml';
   break;
   
   case '' :
-    require __DIR__ . '/views/index.php';
+    require __DIR__ . '/views/index.phtml';
   break;
   
   case '/login' :
-    require __DIR__ . '/views/login.php';
+    require __DIR__ . '/views/login.phtml';
   break;
   
   default:
   http_response_code(404);
-  require __DIR__ . '/views/404.php';
+  require __DIR__ . '/views/404.phtml';
   
 break;
 }
-
-require __DIR__ . '/views/template.php';
